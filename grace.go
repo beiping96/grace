b00867826862039e50c91e49a11ca3cbb74169ef
beiping96 <beiping96@gmail.com>
2019-05-15 16:59:50 +0800

    :sparkles:

--- grace.go
+++ grace.go
@@ -3,16 +3,61 @@ package grace
 import (
 	"context"
 	"os"
+	"sync"
+	"syscall"
 )
 
-type Config struct {
-	StopSignal     []os.Signal
-	RestartSignal  []os.Signal
-	ShutdownSignal []os.Signal
+var (
+	defaultStopSignal = []os.Signal{
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+	}
+)
+
+// Init Declare stop signals
+// Default is syscall.SIGINT or syscall.SIGQUIT
+func Init(stopSignals ...os.Signal) {
+	if len(stopSignals) == 0 {
+		panic("beiping96/grace Init PANIC nil stopSignals")
+	}
+	defaultStopSignal = stopSignals
 }
 
-func Init(cfg *Config) {}
+var (
+	sysGoroutines = []Goroutine{}
+	isRunning     = false
+	cancel        func()
+	ctx           context.Context
+	mu            = new(sync.Mutex)
+	wg            = new(sync.WaitGroup)
+)
 
-func Go(fn func(ctx context.Context)) {}
+// Goroutine Goroutine function
+type Goroutine func(ctx context.Context)
 
-func Run() {}
+// Go Start a goroutine
+func Go(g Goroutine) {
+	if isRunning {
+		go func() {
+			wg.Add(1)
+			defer wg.Done()
+			g(ctx)
+		}()
+		return
+	}
+	mu.Lock()
+	sysGoroutines = append(sysGoroutines, g)
+	mu.Unlock()
+}
+
+// Run Start node
+func Run() {
+	mu.Lock()
+	defer mu.Unlock()
+	isRunning = true
+	ctx, cancel = context.WithCancel(context.Background())
+	go func() {
+
+	}()
+	wg.Wait()
+}
